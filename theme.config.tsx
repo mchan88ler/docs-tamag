172b85105bdb36569631a95f77abde14d1a7d5a7
mchan88ler <142933324+mchan88ler@users.noreply.github.com>
2023-12-30 23:28:00 +0000

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Tamagochi Docs</span>,
+  project: {
+    link: '#',
+  },
+  chat: {
+    link: '#',
+  },
+  docsRepositoryBase: 'https://tamagotchisreinvented.com',
+  footer: {
+    text: 'Tamagotchi Reinvented 2024',
+  },
+}
+
+export default config
